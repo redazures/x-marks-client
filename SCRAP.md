e4da29d29deb7a9e551e539cd7aecc31a7705407
redazures <jwu_nyc@yahoo.com>
2020-09-05 12:48:06 -0400

    updated

---
+++ SCRAP.md
@@ -0,0 +1,17 @@
+function renderCurrentPrice(price){
+    // console.log(price)
+        const actual =["Canadian Dollars","Hong Kong Dollars","Icelandic Krona","Philipino Peso","Danish Krone",]
+        const table = document.getElementById('table-body')
+        const row = document.createElement('tr')
+
+        row.innerHTML =`
+        <td class='api-names'>${actual[0]}</td>
+        <td>${price[0]}</td>
+        <td>${price[1]}</td>
+        <td><button>Edit</button></td>
+        <td><button>Delete</button></td>
+        `
+        table.append(row)
+        // const names = document.querySelectorAll('.api-names')
+        // for(name of names){name.innerContent="x"}
+}
--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const table = document.getElementById('table-body')
 
     const fetchadapter = new FetchAdapter(baseUrl)
-    const action = currencies => (render(currencies.rates))
-    fetchadapter.get('latest?base=USD',action)
-
+    // const action = currencies => (render(currencies.rates)) // is just rendering currencies, you cannot see the API
+    // fetchadapter.get('latest?base=USD',action)
 
     getCurrencies()
 
     function getCurrencies() {
         fetch(railsAPI)
             .then(response => response.json())
-            .then(data => renderCurrencies(data))
+            // .then(data => renderCurrencies(data))
+            .then(string=>string.forEach(currency => renderCurrency(currency)))
     }//getcurrencies
 
     function render (rates){
@@ -41,42 +41,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
             table.append(row)
     }
 
-    function renderCurrency(currency) {
+    function renderCurrency(currency) {//this render curreceny for the initial batch from the system
         const row = document.createElement('tr')
             row.dataset.id = currency.id
-    
             row.innerHTML =`
+            <td>${currency.name}</td>
             <td>${currency.symbol}</td>
             <td>${currency.price}</td>
-            <td><button>Edit</button> <button>Delete</button></td>
-            `
+            <td><button>Buy</button></td>
+            <td><button>Sell</button></td>`
             table.append(row)
-    }
+    }// this is the end of render currency
 
-            
-    function renderCurrencies(currencies) {
-        currencies.forEach(currency => renderCurrency(currency))
-    }
 });
-
-
-
-
-function renderCurrentPrice(price){
-    // console.log(price)
-        const actual =["Canadian Dollars","Hong Kong Dollars","Icelandic Krona","Philipino Peso","Danish Krone",]
-        const table = document.getElementById('table-body')
-        const row = document.createElement('tr')
-
-        row.innerHTML =`
-        <td class='api-names'>${actual[0]}</td>
-        <td>${price[0]}</td>
-        <td>${price[1]}</td>
-        <td><button>Edit</button></td>
-        <td><button>Delete</button></td>
-        `
-        table.append(row)
-        // const names = document.querySelectorAll('.api-names')
-        // for(name of names){name.innerContent="x"}
-
-}
