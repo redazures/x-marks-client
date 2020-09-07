d18a4bd774f8a98aa55bd1bb17462be4c7803eee
redazures <jwu_nyc@yahoo.com>
2020-09-07 14:06:42 -0400

    parts

--- src/main.js
+++ src/main.js
@@ -18,15 +18,15 @@ function renderCurrency(currency) {//this render curreceny for the initial batch
   const allList = document.querySelector('.add-currency-list')
   const li = document.createElement('li')
   li.dataset.id=currency.id
-  li.innerHTML=`<img src="https://freeiconshop.com/wp-content/uploads/edd/plus-flat.png" class="flag"><span>${currency.symbol}-${currency.name} <p>${currency.price}<p></span>`
+  li.innerHTML=`<img src="https://freeiconshop.com/wp-content/uploads/edd/plus-flat.png" class="flag"><span>${currency.symbol}-${currency.name}</span>`
   allList.appendChild(li)
 }// this is the end of render currency
 
 
-
+function addButton(){
   const add = document.querySelector('.add-currency-btn')
-    let tog = {
-      "add-currency-btn": "add-currency-btn open",
-      "add-currency-btn open": "add-currency-btn"
-    }
-
+    
+  add.addEventListener('click',(e)=>{
+    console.log("you are at add")
+  })//This is the end of my addButton
+}//this is the end of my button
